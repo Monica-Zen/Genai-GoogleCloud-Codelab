d8aa333a54142b0fd2188ea1ea13900a242c1fc0
Monica-Zen <monika15verma@gmail.com>
2025-01-22 19:56:15 +0000

    add latest changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 import { genkit } from 'genkit';
-import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+import { vertexAI } from '@genkit-ai/vertexai';
 
 const ai = genkit({
     plugins: [
@@ -12,11 +12,8 @@ const ai = genkit({
 
 app.get('/', async (req, res) => {
     const animal = req.query.animal || 'dog';
-    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
-    const llmResponse = await ai.generate({
-        model: gemini15Flash,
-        prompt: prompt,
-    });
+    const animalPrompt = ai.prompt('animal-facts');
+    const llmResponse = await animalPrompt({animal});
     const html = llmResponse.text;
     res.send(html);
 });
@@ -25,3 +22,32 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`codelab-genai: listening on port ${port}`);
 });
+
+
+// import express from 'express';
+// const app = express();
+
+// import { genkit } from 'genkit';
+// import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+
+// const ai = genkit({
+//     plugins: [
+//         vertexAI({ location: 'us-central1' }),
+//     ],
+// });
+
+// app.get('/', async (req, res) => {
+//     const animal = req.query.animal || 'dog';
+//     const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+//     const llmResponse = await ai.generate({
+//         model: gemini15Flash,
+//         prompt: prompt,
+//     });
+//     const html = llmResponse.text;
+//     res.send(html);
+// });
+
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//     console.log(`codelab-genai: listening on port ${port}`);
+// });
